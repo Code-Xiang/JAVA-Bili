91113ca94356a2f239ef3ca0603eedb43a5773c0
Code-Xiang <932027769@qq.com>
2023-11-09 23:28:29 +0800

    HP Linux

--- springboot-02-config/src/main/java/com/kuang/config/MyMvcConfig.java
+++ springboot-02-config/src/main/java/com/kuang/config/MyMvcConfig.java
@@ -1,7 +1,9 @@
 package com.kuang.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.*;
 
 /**
@@ -14,6 +16,20 @@ public class MyMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/main.html").setViewName("dashboard");
+
+    }
+//    自定义国际组件
+    @Bean
+    public LocaleResolver localeResolver() {
+        return  new MyLocaleResolver();
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/index.html","/","/user/login","/css/*","/js/**","/img/**");
+    }
 }
---
+++ springboot-02-config/src/main/java/com/kuang/controller/EmployeeController.java
@@ -0,0 +1,41 @@
+package com.kuang.controller;
+
+import com.kuang.dao.DepartmentDao;
+import com.kuang.dao.EmployeeDao;
+import com.kuang.pojo.Department;
+import com.kuang.pojo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
+    @RequestMapping("/emps")
+    public String list(Model model){
+        Collection<Employee> employees = employeeDao.getAll();
+        model.addAttribute("emps", employees);
+        return "emp/list";
+    }
+    @GetMapping("/emp")
+    public String toAddpage(Model model) {
+    // 查出所有部门的信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments", departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Model model) {
+    //   TODO     添加的操作
+        return "redirect:/emps";
+    }
+}
